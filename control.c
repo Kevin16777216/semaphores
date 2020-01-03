38f967070929a873449a417aa169a77f2a4ca00c
Kevin Cai <k1029384756c@gmail.com>
2020-01-03 05:53:33 -0500

    Update control.c

--- control.c
+++ control.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+v\#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <sys/types.h>
 #include <sys/shm.h>
+#include <sys/types.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
@@ -13,13 +13,19 @@
 #include <unistd.h>
 
 #define KEY 24602
-#define SIZE 200
+#define SIZE 255
 union semun {
     int val;
     struct semid_ds *buf;
     unsigned short  *array;
     struct seminfo  *__buf;
 };
+void rd(){
+  FILE* file = fopen("m.txt", "r");
+  char line[SIZE];
+  while(fgets(line, SIZE, file))
+      printf("%s", line);
+}
 char ** parse_args( char * line ) {
     char *token;
     char **out = malloc(strlen(line) * sizeof(char *));
@@ -31,7 +37,7 @@ char ** parse_args( char * line ) {
     return out;
 }
 int main(int argc, char * argv[]) {
-    FILE *f;
+    FILE *file;
     char* data;
     char input[256] = "";
     int j;
@@ -52,46 +58,38 @@ int main(int argc, char * argv[]) {
         printf("Error : need to have input\n");
         exit(0);
     }
+    int sem,shm;
     if(!strcmp(in[0],"-c")) {
-        int semd,shmd,r,v;
+        int r,v;
         char* data;
-        semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-        if (semd == -1) {
-            semd = semget(KEY, 1, 0);
-            v = semctl(semd, 0, GETVAL, 0);
-            printf("Semaphore already exists.\n");
+        sem = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+        if (sem == -1) {
+            sem = semget(KEY, 1, 0);
+            v = semctl(sem, 0, GETVAL, 0);
+            printf("semaphore already exists\n");
         } else {
             union semun us;
             us.val = 1;
-            r = semctl(semd, 0, SETVAL, us);
-            printf("semaphore created.\n");
+            r = semctl(sem, 0, SETVAL, us);
+            printf("semaphore created\n");
         }
-        shmd = shmget(KEY, SIZE, IPC_CREAT | 0644);
-        printf("shared memory created.\n");
-        data = shmat(shmd, 0, 0);
-        f = fopen("m.txt","w");
+        shm = shmget(KEY, SIZE, IPC_CREAT | 0644);
+        printf("shared memory created\n");
+        data = shmat(shm, 0, 0);
+        file = fopen("m.txt","w");
         printf("file created.\n");
-
     } else if(!strcmp(in[0],"-r")) {
         printf("trying to get in\n");
-        int semd = semget(KEY, 1, 0);
-        int shmd = shmget(KEY, 1, 0);
-        shmctl(shmd, IPC_RMID, 0);
+        sem = semget(KEY, 1, 0);
+        shm = shmget(KEY, 1, 0);
+        shmctl(shm, IPC_RMID, 0);
         printf("shared memory removed\n");
-        semctl(semd, IPC_RMID, 0);
-        f = fopen("m.txt", "r");
-        char line[SIZE];
-        while(fgets(line, SIZE, f)) {
-            printf("%s", line);
-        }
+        semctl(sem, IPC_RMID, 0);
+        rd();
         remove("m.txt");
-        printf("file removed.\nsemaphore removed.\n");
+        printf("file removed\nsemaphore removed\n");
     } else if(!strcmp(in[0],"-v")) {
-        f = fopen("m.txt", "r");
-        char line[SIZE];
-        while(fgets(line, SIZE, f)) {
-            printf("%s", line);
-        }
+        rd();
     } else {
         printf("Error : invalid input");
         exit(0);
